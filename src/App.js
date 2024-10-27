2db11b08cb06185391e9ecb390df7c94d1098f7b
Jose Ruiz <38330657+jruiiz@users.noreply.github.com>
2024-10-26 21:45:02 -0400

    feat: Create NavigationBar component

--- src/App.js
+++ src/App.js
@@ -4,38 +4,14 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import ForumPage from './components/forumPage'; // Ensure the path is correct
 
+// Components import
+import NavigationBar from './components/navigation-bar/NavigationBar';
+
 function App() {
   return (
     <Router>
       {/* Navigation Bar */}
-      <nav className="navbar">
-        <div className="navbar-brand">
-          <Link to="/" className="brand-logo">
-            Lost & Found
-          </Link>
-        </div>
-        <ul className="navbar-list">
-          <li className="navbar-item">
-            <Link to="/">Home</Link>
-          </li>
-          <li className="navbar-item">
-            <Link to="/forum">View Posts</Link>
-          </li>
-          <li className="navbar-item">
-            <Link to="/logs">Check Logs</Link> 
-          </li>
-        </ul>
-
-        {/* Search Bar */}
-        <div className="navbar-search">
-          <input
-            type="text"
-            placeholder="Search with keywords"
-            className="search-input"
-          />
-          <button className="search-button">Search</button>
-        </div>
-      </nav>
+      <NavigationBar />
 
       {/* Main Content */}
       <Routes>
---
+++ src/components/navigation-bar/NavigationBar.jsx
@@ -0,0 +1,36 @@
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+
+function NavigationBar() {
+    return(
+        <nav className="navbar">
+            <div className="navbar-brand">
+            <Link to="/" className="brand-logo">
+                Lost & Found
+            </Link>
+            </div>
+            <ul className="navbar-list">
+            <li className="navbar-item">
+                <Link to="/">Home</Link>
+            </li>
+            <li className="navbar-item">
+                <Link to="/forum">View Posts</Link>
+            </li>
+            <li className="navbar-item">
+                <Link to="/logs">Check Logs</Link> 
+            </li>
+            </ul>
+
+            {/* Search Bar */}
+            <div className="navbar-search">
+            <input
+                type="text"
+                placeholder="Search with keywords"
+                className="search-input"
+            />
+            <button className="search-button">Search</button>
+            </div>
+      </nav>
+    );
+}
+
+export default NavigationBar
