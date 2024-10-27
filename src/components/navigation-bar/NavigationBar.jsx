2bc268f6f7906e74ddc172017106b0b45034532c
Jose Ruiz <38330657+jruiiz@users.noreply.github.com>
2024-10-26 23:14:26 -0400

    feat: Apply nav-bar internal styles

--- src/components/navigation-bar/NavigationBar.jsx
+++ src/components/navigation-bar/NavigationBar.jsx
@@ -1,33 +1,41 @@
 import { Link } from 'react-router-dom';
+import styles from "./NavigationBar.module.css"
+
 
 function NavigationBar() {
     return(
-        <nav className="navbar">
-            <div className="navbar-brand">
-            <Link to="/" className="brand-logo">
-                Lost & Found
-            </Link>
+        <nav className={styles.navBar}>
+
+            {/* Website name and logo */}
+            <div className={styles.navBarBrand}>
+                {/* 
+                    Add the GMU logo here in the future with a vertical line 
+                    splitting the logo and "Lost and found" text
+                */}
+                <Link to="/" className={styles.brandLogo}>Lost & Found</Link>
             </div>
-            <ul className="navbar-list">
-            <li className="navbar-item">
+
+            {/* Links to other pages */}
+            <ul className={styles.navBarList}>
+            <li className={styles.navBarItem}>
                 <Link to="/">Home</Link>
             </li>
-            <li className="navbar-item">
+            <li className={styles.navBarItem}>
                 <Link to="/forum">View Posts</Link>
             </li>
-            <li className="navbar-item">
+            <li className={styles.navBarItem}>
                 <Link to="/logs">Check Logs</Link> 
             </li>
             </ul>
 
             {/* Search Bar */}
-            <div className="navbar-search">
+            <div className={styles.navBarSearch}>
             <input
                 type="text"
                 placeholder="Search with keywords"
-                className="search-input"
+                className={styles.searchInput}
             />
-            <button className="search-button">Search</button>
+            <button className={styles.searchButton}>Search</button>
             </div>
       </nav>
     );
