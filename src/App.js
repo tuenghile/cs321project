b2331d5c17115b52d847be43e3b1150cc0d927a4
Jose Ruiz <38330657+jruiiz@users.noreply.github.com>
2024-10-27 02:46:43 -0400

    chore: Add homepage element for testing

--- src/App.js
+++ src/App.js
@@ -7,14 +7,15 @@ import ForumPage from './pages/forumPage'; // Ensure the path is correct
 // Components import
 import NavigationBar from './components/navigation-bar/NavigationBar';
 import HomePage from './pages/homePage';
+import PageHeader from './components/page-header/PageHeader';
 
 function App() {
   return (
     <Router>
       {/* Navigation Bar */}
       <NavigationBar />
-      <HomePage></HomePage>
-
+      {/* <PageHeader websiteName="George Mason University" pageName="LOST AND FOUND" /> */}
+      <HomePage />
       {/* Main Content */}
       <Routes>
         <Route path="/" element={<h1>Home Page</h1>} /> {/* Placeholder for home page */}
