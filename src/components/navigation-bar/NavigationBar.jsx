31bca631c374d1c20d9142c5b499d09477c2a1eb
Jose Ruiz <38330657+jruiiz@users.noreply.github.com>
2024-10-26 22:34:57 -0400

    refactor: Update import statement for links

--- src/components/navigation-bar/NavigationBar.jsx
+++ src/components/navigation-bar/NavigationBar.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function NavigationBar() {
     return(
--- src/components/recent-posts/RecentPosts.jsx
+++ src/components/recent-posts/RecentPosts.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function RecentPosts() {
     const [recentPosts, setRecentPosts] = useState([]);
