3fb210dd0ebdf6171e3d8a659a35960a798f7602
Jose Ruiz <38330657+jruiiz@users.noreply.github.com>
2024-10-26 22:14:20 -0400

    feat: Add contact section as component

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -1,13 +1,14 @@
 import './css/homePage.css';
-import mapImage from '../assets/map.png';
 import PageHeader from '../components/page-header/PageHeader';
 import RecentPosts from '../components/recent-posts/RecentPosts';
+import ContactSection from '../components/contact-section/ContactSection';
 
 const HomePage = () => {
   
 
   return (
     <div className="home-page">
+
       {/* Header Section */}
       <PageHeader websiteName="George Mason University" pageName="LOST AND FOUND" />
 
@@ -15,22 +16,8 @@ const HomePage = () => {
       <RecentPosts />
 
       {/* Contact Section */}
-      <section className="contact-section">
-        <div className="contact-box">
-          <div className="contact-info">
-            <h3>Have Any Questions? Contact Us</h3>
-            <p><strong>Phone:</strong> (703)-993-2975</p>
-            <p><strong>Email:</strong> property@gmu.edu</p>
-            <p><strong>Address:</strong> 4400 University Drive, MSN 2FL, Fairfax, Virginia 22030</p>
-          </div>
-          <div className="contact-map">
-            <img
-              src={mapImage}
-              alt="GMU Campus Map"
-            />
-          </div>
-        </div>
-      </section>
+      <ContactSection />
+
     </div>
   );
 };
