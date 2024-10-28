9066b253b84e8a601d89e853dc13404808248499
Jose Ruiz <38330657+jruiiz@users.noreply.github.com>
2024-10-28 19:54:20 -0400

    feat: Remove dynamic card code for testing

--- src/components/recent-posts-section/RecentPostsSection.jsx
+++ src/components/recent-posts-section/RecentPostsSection.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import PrimaryButton from '../primary-button/PrimaryButton';
 import styles from "../recent-posts-section/RecentPostsSection.module.css"
-// import PostCard from '../post-card/PostCard';
+import PostCard from '../post-card/PostCard';
 
 function RecentPostsSection() {
     const [recentPosts, setRecentPosts] = useState([]);
@@ -22,41 +22,13 @@ function RecentPostsSection() {
 
           <div className={styles.postsGrid}>
             {/* Testing post cards in the recent post section */}
-            {/* <PostCard />
             <PostCard />
             <PostCard />
             <PostCard />
             <PostCard />
-            <PostCard /> */}
-            
-            {recentPosts.length > 0 ? (
-              recentPosts.map((post) => (
-                <div
-                  key={post.id}
-                  className={styles.postCard}
-                  style={{
-                    borderLeft: `5px solid ${
-                      post.reportType === 'Lost' ? '#a81d31' : '#FFA500'
-                    }`,
-                  }}
-                >
-                    {/* Code for the post card (make this component) */}
-                  <h3>{post.title}</h3>
-                  <p><strong>Location:</strong> {post.location}</p>
-                  {post.description && <p>{post.description}</p>}
-                  {post.image && (
-                    <img
-                      src={URL.createObjectURL(post.image)}
-                      alt={post.title}
-                      className={styles.postImage}
-                    />
-                  )}
+            <PostCard />
+            <PostCard />
 
-                </div>
-              ))
-            ) : ( // Print this if no posts available
-              <p>No recent posts available.</p>
-            )}
           </div>
 
           <Link to="/forum" style={{ textDecoration: 'none' }}>
