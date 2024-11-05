0b44003e8bc84a6cce207008d99c348d602903b2
Jose Ruiz <38330657+jruiiz@users.noreply.github.com>
2024-11-04 20:21:29 -0500

    feat: Apply different structure to contact section

--- src/components/contact-section/ContactSection.jsx
+++ src/components/contact-section/ContactSection.jsx
@@ -1,16 +1,20 @@
 import mapImage from "../../assets/map.png"
+import styles from "./ContactSection.module.css"
 
 function ContactSection() {
     return(
-        <section className="contact-section" id="contact">
-            <div className="contact-box">
-                <div className="contact-info">
-                    <h3>Have Any Questions? Contact Us</h3>
-                    <p><strong>Phone:</strong> (703)-993-2975</p>
-                    <p><strong>Email:</strong> property@gmu.edu</p>
-                    <p><strong>Address:</strong> 4400 University Drive, MSN 2FL, Fairfax, Virginia 22030</p>
+        <section className={styles.contactSection} id="contact">
+            <h2>Contact</h2>
+            <div className={styles.contactContent}>
+                <div className={styles.contactBox}>
+                    <div className={styles.contactInfo}>
+                        <h3>Have Any Questions? Reach out to us!</h3>
+                        <p><strong>Phone:</strong> (703)-993-2975</p>
+                        <p><strong>Email:</strong> property@gmu.edu</p>
+                        <p><strong>Address:</strong> 4400 University Drive, MSN 2FL, Fairfax, Virginia 22030</p>
+                    </div>
                 </div>
-                <div className="contact-map">
+                <div className={styles.contactMap}>
                     <img
                     src={mapImage}
                     alt="GMU Campus Map"
