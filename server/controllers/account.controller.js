d14ab13025742fe99b60dc74ceb22922a9d52383
Victor Do <victdo.03@gmail.com>
2024-11-09 15:02:39 -0500

    added a get user route

--- server/controllers/account.controller.js
+++ server/controllers/account.controller.js
@@ -22,6 +22,18 @@ const createAccount = async (req, res) => {
     }
 }
 
+// finds user using their email
+const getAccount = async (req, res) => {
+    try{
+        const account = await Account.findOne({ "email": req.body.email });
+        res.status(200).json(account); // returns json with user information
+    }
+    catch (error){
+        res.status(500).json({message: error.message});
+    }
+}
+
 module.exports = {
-    createAccount
+    createAccount,
+    getAccount
 };
--- server/routes/account.route.js
+++ server/routes/account.route.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const { createAccount } = require("../controllers/account.controller.js");
+const { createAccount, getAccount } = require("../controllers/account.controller.js");
 
 // create account
 router.post("/create/", createAccount);
+
+// find user id
+router.get("/getuser", getAccount);
+
 
 module.exports = router;
