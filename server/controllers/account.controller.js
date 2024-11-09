1867da4e9a82cc337c63769336d5cebbfb74c475
Victor Do <victdo.03@gmail.com>
2024-11-09 15:40:44 -0500

    changed account schema to support both admin and user accounts

--- server/controllers/account.controller.js
+++ server/controllers/account.controller.js
@@ -5,12 +5,14 @@ const createAccount = async (req, res) => {
         const name = req.body.name;
         const email = req.body.email; // TODO: verify email
         const password = req.body.password; // TODO: add encryption
+        const type = req.body.type;
         const gnumber = req.body.gnumber; 
 
         const accountInfo = {
             name,
             email,
             gnumber,
+            type,
             password
         }
         const account = await Account.create(accountInfo);
