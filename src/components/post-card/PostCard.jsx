4f4924c52fdcb1a1a35344eac02e6dc0b2989c40
Jose Ruiz <38330657+jruiiz@users.noreply.github.com>
2024-11-21 07:46:54 -0500

    feat: Change placeholder image

--- src/components/post-card/PostCard.jsx
+++ src/components/post-card/PostCard.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import styles from "../post-card/PostCard.module.css";
-import tempPostImage from "../../assets/map.png";
+import tempPostImage from "../../assets/placeholder-img.png";
 
 function PostCard({
   cardTitle = "Post Title",
