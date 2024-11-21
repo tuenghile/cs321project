4720e56b134be62869554a81295bb58fe87d1ce6
Jose Ruiz <38330657+jruiiz@users.noreply.github.com>
2024-11-21 06:46:36 -0500

    feat: Add condition to go to settings if logged in

--- src/components/navigation-bar/NavigationBar.jsx
+++ src/components/navigation-bar/NavigationBar.jsx
@@ -3,6 +3,8 @@ import gmuLogo from "../../assets/gmu-logo.png"
 import { HashLink as Link } from 'react-router-hash-link';
 import accountIcon from "../../assets/user.png"
 import HamburgerMenu from "../hamburger-menu/HamburgerMenu";
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom'; 
 
 // This is so that we scroll a bit higher than it had before. It was clipping the page heading.
 // Can use this for other pages to scroll to top of the page instead of clipping the heading, or offsets for other sections
@@ -12,7 +14,39 @@ const scrollOffset = (el) => {
     window.scrollTo({top: y, behavior: "smooth"});
 };
 
+
 function NavigationBar() {
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+    // Check if user is logged in
+    useEffect(() => {
+    fetch("http://localhost:3002/account/", {
+        method: "GET",
+        credentials: "include"
+    })
+        .then((response) => {
+        if (response.ok) {
+            setIsAuthenticated(true);
+        } else {
+            setIsAuthenticated(false);
+        }
+        })
+        .catch((error) => {
+        console.error("Error checking authentication:", error);
+        setIsAuthenticated(false);
+        });
+    }, []);
+
+    const navigate = useNavigate(); // Initialize navigate function
+    // Using this to choose the right page based on the login state and clicking the account icon
+    const handleAccountClick = () => {
+        if (isAuthenticated) {
+            navigate("/settings"); // Replace with your desired authenticated route
+        } else {
+            navigate("/login");
+        }
+    };
+
     return(
         <header className={styles.navBar}>
             {/* 
@@ -63,13 +97,9 @@ function NavigationBar() {
                         placeholder="Search with keywords"
                         className={styles.searchInput}
                     />
-                <Link to="/login">
-                    <div className={styles.accountButton}>
-                        <img src={accountIcon} alt="account/user icon" height="40px"/>
-
-                    </div>
-                </Link>
-
+                <div onClick={handleAccountClick} className={styles.accountButton}>
+                    <img src={accountIcon} alt="account/user icon" height="40px"/>
+                </div>
                 </div>
             </div>
 
